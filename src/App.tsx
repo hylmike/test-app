e301aa0f4c1706de58368c1692994469eec53c1b
Michael Hua <huayl.michael@gmail.com>
2021-12-10 15:13:16 -0500

    V0.1

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,40 @@ import React from "react";
 import "./App.scss";
 import PictureItem from "./components/PictureItem";
 import { pictureList } from "./data-sample/picture-list";
+import { FixedSizeGrid as Grid } from "react-window";
+
+type Props = {
+  columnIndex: number;
+  rowIndex: number;
+};
 
 function App() {
+  const Cell = ({ columnIndex, rowIndex }: Props) => {
+    const index = (rowIndex + 1) * (columnIndex + 1) - 1;
+    return (
+      <div className="col-md-4">
+        <PictureItem
+          pictureUrl={pictureList[index].picture_url}
+          name={pictureList[index].title}
+        />
+      </div>
+    );
+  };
+
   return (
     <div className="App bg-dark">
       <h2 className="title">Taft- Multimedia Gallery</h2>
-      <div className="pic-container row gx-0 gy-0 py-0">
-        {pictureList.map((item) => (
-          <div className="col-md-4">
-            <PictureItem
-              pictureUrl={item.picture_url}
-              name={item.title}
-              key={item.id}
-            />
-          </div>
-        ))}
+      <div className="pic-container row">
+        <Grid
+          columnCount={3}
+          columnWidth={300}
+          width={1000}
+          height={800}
+          rowHeight={200}
+          rowCount={Math.ceil(pictureList.length / 3)}
+        >
+          {Cell}
+        </Grid>
       </div>
     </div>
   );
